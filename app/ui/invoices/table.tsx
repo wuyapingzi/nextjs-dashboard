4ffb17ed6b3c4ac3f9335d72d043f1cc15ba9f1f
wuyaping <wuyaping@licaigc.com>
2024-10-31 16:00:25 +0800

    lint

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -47,8 +47,8 @@ export default async function InvoicesTable({
                     <p>{formatDateToLocal(invoice.date)}</p>
                   </div>
                   <div className="flex justify-end gap-2">
-                    <UpdateInvoice id={invoice.id} />
-                    <DeleteInvoice id={invoice.id} />
+                    <UpdateInvoice />
+                    <DeleteInvoice />
                   </div>
                 </div>
               </div>
